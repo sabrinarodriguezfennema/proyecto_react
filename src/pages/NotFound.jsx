8ee2d7b729da68cc06154a462ce6d4531a9fc910
sabrinafennema <sabrifennema@gmail.com>
2025-05-24 18:25:50 -0300

    añadi estilo a la pag not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./styles/NotFound.css";
 import { Link } from "react-router-dom";
-import loading from '../assets/loading.gif'
 
-const NotFound = (cargando) => {
+
+const NotFound = () => {
   return (
     <main>
-      {cargando ? <img src={loading} alt='loading' style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999', width: '50px', height: '50px' }} /> :
-        <div>
-          <h1>404</h1>
-          <button><Link to='/'>Volver Al Inicio</Link></button>
+      <div className="contenedor">
+          <h1 className="titulo">404</h1>
+          <p>Página no encontrada</p>
+          <button className="boton"><Link className="link" to='/'>Volver Al Inicio</Link></button>
         </div>
-      }
+      
     </main>
 
   );
