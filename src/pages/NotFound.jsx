8f5d5548e4e90eaff2589ee7ad69f82fbde20b4c
sabrinafennema <sabrifennema@gmail.com>
2025-05-24 17:56:49 -0300

    cambios

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import "./styles/NotFound.css";
 import { Link } from "react-router-dom";
 import loading from '../assets/loading.gif'
 
-const NotFound = () => {
+const NotFound = (cargando) => {
   return (
     <main>
       {cargando ? <img src={loading} alt='loading' style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999', width: '50px', height: '50px' }} /> :
